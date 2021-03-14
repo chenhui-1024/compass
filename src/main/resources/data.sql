d8f1d042fdca5206ba0620bc89d06da6660ce94e
chenhui-1024 <allinchenhui@163.com>
2021-03-14 17:43:59 +0800

    upload release jar

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,9 @@ drop table if exists compass_decision_table;
 create table compass_decision_table(
 id varchar(120) not null primary key,
 name varchar(120),
-description varchar(140),
-input_variables_json varchar(1024),
-output_variables_json varchar(1024),
+description varchar(512),
+input_variables_json varchar(2048),
+output_variables_json varchar(2048),
 rules_json varchar(2048),
 status varchar(24),
 version int default 0,
@@ -18,9 +18,9 @@ update_time varchar(12)
 drop table if exists compass_account;
 
 create table compass_account(
-id varchar(50),
+id varchar(50) not null primary key,
 username varchar(12),
-email varchar(14),
+email varchar(50),
 pwd_salt varchar(30),
 hashed_pwd varchar(100),
 create_time varchar(20)
